881d54fe5867f6b4aaac62d41be5d476a85f7956
Imed Kouki <88271652+being3iimed@users.noreply.github.com>
2023-07-07 17:22:04 +0200

    Create BuySellStock.java

---
+++ BuySellStock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+      int buy = 0, sell = 1, profit = 0, MaxProf = 0;
+      while(sell < prices.length){
+          if(prices[buy] > prices[sell]){
+              buy = sell;
+          }
+          else if(prices[buy] < prices[sell]){
+              profit = (prices[sell] - prices[buy]);
+              MaxProf = Math.max(MaxProf, profit);
+          }
+          sell++;
+      }   
+      return MaxProf;
+    }
+}
